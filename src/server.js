3d75e8a770f8c8da19ff287cd4e58ca864d7032d
averus9 <16520480@std.stei.itb.ac.id>
2021-08-14 00:55:07 +0700

    backend

---
+++ src/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+let ejs = require('ejs');
+var path = require('path');
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+const meet = [[1,2,3],[2,3]]
+app.get('/', (req, res) => {
+    res.render(__dirname+ '/views/contoh', {
+        namas: meet
+    });
+});
+const data = {
+    ttki: 
+    {
+        nama: "Tata tulis karya ilmiah",
+        semester: 1,
+        files: [{nama:'Buku sakti', link:'drive.com'},{nama: 'Buku besar', link:'sipsipsip'}],
+        subbab: [
+            {
+                nama: "Kata Baku",
+                materi: [{nama: 'Kata baku non esensial', link:'youtube.com/sasasa atau drive atau pdf atau quiziz'},{nama:'Kata baku esensial', link:'youtube.com/asasafd'}]
+            },
+            {
+                nama: "Kata Baku",
+                materi: [{nama: 'Kata baku non esensial', link:'youtube.com/sasasa atau drive atau pdf atau quiziz'},{nama:'Kata baku esensial', link:'youtube.com/asasafd'}]
+            },
+            {
+    
+            }
+        ]
+    },
+}
+app.get('/:matkul', (req, res) => {
+    const matkul = req.params.matkul;
+    res.render(__dirname+'/views/matkul', {
+        matkul: data[matkul]
+    })
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server aktif');
+});
