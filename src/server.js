6b82cc21a9f20713ea6811d13abeddf63fe594d0
sumertayogaa <16520482@std.stei.itb.ac.id>
2021-08-14 15:57:12 +0800

    perubahan minor di subbab

--- src/server.js
+++ src/server.js
@@ -3,52 +3,55 @@ const app = express();
 let ejs = require('ejs');
 var path = require('path');
 
-
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-app.use( express.static( "public" ) );
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/home.html')
-})
+  res.sendFile(__dirname + '/views/home.html');
+});
 
-const meet = [[1,2,3],[2,3]]
+const meet = [
+  [1, 2, 3],
+  [2, 3],
+];
 app.get('/meet', (req, res) => {
-    res.render(__dirname+ '/views/meet', {
-        meets: meet
-    });
+  res.render(__dirname + '/views/meet', {
+    meets: meet,
+  });
 });
 const data = {
-    ttki: 
-    {
-        nama: "Tata tulis karya ilmiah",
-        semester: 1,
-        files: [{nama:'Buku sakti', link:'drive.com'},{nama: 'Buku besar', link:'sipsipsip'}],
-        subbab: [
-            {
-                nama: "Kata Baku",
-                materi: [{nama: 'Kata baku non esensial', link:'youtube.com/sasasa atau drive atau pdf atau quiziz'},{nama:'Kata baku esensial', link:'youtube.com/asasafd'}]
-            },
-            {
-                nama: "Kata Baku",
-                materi: [{nama: 'Kata baku non esensial', link:'youtube.com/sasasa atau drive atau pdf atau quiziz'},{nama:'Kata baku esensial', link:'youtube.com/asasafd'}]
-            },
-            {
-            
-            }
-        ]
-    },
-}
-app.get('/:matkul', (req, res) => {
-    const matkul = req.params.matkul;
-    res.render(__dirname+'/views/matkul', {
-        matkul: data[matkul]
-    })
-})
-
-
+  ttki: {
+    nama: 'Tata tulis karya ilmiah',
+    semester: 1,
+    files: [
+      { nama: 'Buku sakti', link: 'drive.com' },
+      { nama: 'Buku besar', link: 'sipsipsip' },
+    ],
+    subbab: [
+      {
+        nama: 'Kata Baku',
+        materi: [
+          { nama: 'Kata baku non esensial', link: 'youtube.com/sasasa atau drive atau pdf atau quiziz' },
+          { nama: 'Kata baku esensial', link: 'youtube.com/asasafd' },
+        ],
+      },
+      {
+        nama: 'Kata Baku',
+        materi: [
+          { nama: 'Kata baku non esensial', link: 'youtube.com/sasasa atau drive atau pdf atau quiziz' },
+          { nama: 'Kata baku esensial', link: 'youtube.com/asasafd' },
+        ],
+      },
+    ],
+  },
+};
+app.get('/subbab', (req, res) => {
+  res.render(__dirname + '/views/subbab', {
+    matkul: data['ttki'],
+  });
+});
 
 app.listen(3000, () => {
-    console.log('Server aktif');
+  console.log('Server aktif');
 });
