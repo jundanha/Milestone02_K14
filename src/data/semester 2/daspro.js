a673e5f33d7909af5665df5214c07668029bc038
averus9 <16520480@std.stei.itb.ac.id>
2021-08-14 17:45:56 +0700

    daspro unfinished tapi pgn pindah branch

---
+++ src/data/semester 2/daspro.js
@@ -0,0 +1,48 @@
+module.exports = {
+    daspro: 
+    {
+        files: [{nama:'Buku sakti', link:'drive.com'},{nama: 'Buku besar', link:'sipsipsip'}],
+        subbab: [
+            {
+                nama: "Konsep dan Lingkungan Pemrograman",
+                materi: [{nama: 'What is programming fundamentals concept?', link:''},{nama:'Programming tools and environment', link:''},{nama:'Peran bahasa pemrograman', link:''}]
+            },
+            {
+                nama: "Berpikir Abstrak dan Fungsional untuk Problem Solving",
+                materi: [{nama: 'Berpikir abstrak dan fungsional', link:''},{nama:'Pengantar rekurens', link:''},{nama:'Rekurens dan pemrograman fungsional: Operasi max dan min', link:''},{nama:'Rekurens dan pemrograman fungsional: ekspresi lambda', link:''}]
+            },
+            {
+                nama: "Berpikir Modular, Dekomposisi Modul, dan Pemrograman Prosedural",
+                materi: [{nama: 'Pengenalan pola', link:''},{nama:'Generalisasi pola untuk mendefinisikan abstraksi atau model', link:''},{nama:'Source code standards and best practices of programming', link:''},{nama:'Program taxonomy dan kemampuan programming yang dibutuhkan', link:''}]
+            },
+            {
+                nama: "Tata Istilah, Definisi, dan Silogisme",
+                materi: [{nama: 'Pembentukan dan pemakaian istilah', link:''},{nama:'Penyusunan definisi', link:''}, {nama:'Penarikan simpulan', link:''}]
+            },
+            {
+                nama: "Paragraf",
+                materi: [{nama: 'Syarat paragraf', link:''},{nama:'Jenis paragraf', link:''},{nama:'Pengembangan paragraf', link:''}]
+            },
+            {
+                nama: "Keplagiatan",
+                materi: [{nama: 'Jenis keplagiatan', link:''},{nama:'Pencegahan keplagiatan', link:''}]
+            },
+            {
+                nama: "Karya Tulis Ilmiah",
+                materi: [{nama: 'Kriteria ilmiah', link:''},{nama:'Pemilihan topik', link:''}, {nama:'Penentuan tema', link:''}, {nama:'Penyusunan karangan', link:''}]
+            },
+            {
+                nama: "Komponen Karangan",
+                materi: [{nama: 'Komponen pelengkap awal', link:''},{nama:'Kompoonen isi', link:''}, {nama: 'Komponen pelengkap akhir', link:''}]
+            },
+            {
+                nama: "Bagian Utama Karangan",
+                materi: [{nama: 'Bab pendahuluan', link:''},{nama:'Bab permasalahan', link:''},{nama:'Bab analisis', link:''},{nama:'Bab simpulan dan saran', link:''}]
+            },
+            {
+                nama: "Konvensi Naskan",
+                materi: [{nama: 'Pengetikan', link:''},{nama:'Tipografi', link:''},{nama:'Pengutipan', link:''},{nama:'Pustaka', link:''}]
+            }
+        ]
+    }
+}
