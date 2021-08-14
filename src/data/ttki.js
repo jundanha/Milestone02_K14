a366d94c15f65eba309744354540f2d4f3421f43
averus9 <16520480@std.stei.itb.ac.id>
2021-08-14 22:59:42 +0700

    ttki

--- src/data/ttki.js
+++ src/data/ttki.js
@@ -1,16 +1,49 @@
 module.exports = {
     ttki: 
     {
+        semester: 1,
         files: [{nama:'Buku sakti', link:'drive.com'},{nama: 'Buku besar', link:'sipsipsip'}],
         subbab: [
             {
-                nama: "Kata Baku",
-                materi: [{nama: 'Kata baku non esensial', link:'youtube.com/sasasa atau drive atau pdf atau quiziz'},{nama:'Kata baku esensial', link:'youtube.com/asasafd'}]
+                nama: "Ejaan",
+                materi: [{nama: 'Penulisan huruf, kata, dan unsur serapan', link:''},{nama:'Pemakaian tanda baca', link:''}]
             },
             {
-                nama: "Kata Baku",
-                materi: [{nama: 'Kata baku non esensial', link:'youtube.com/sasasa atau drive atau pdf atau quiziz'},{nama:'Kata baku esensial', link:'youtube.com/asasafd'}]
+                nama: "Morfologi",
+                materi: [{nama: 'Pembentukan kata', link:''},{nama:'Pemakaian kata bentukan dalam kalimat', link:''}]
             },
+            {
+                nama: "Tata Kalimat",
+                materi: [{nama: 'Pola kalimat baku', link:''},{nama:'Kalimat efektif dan variasi kalimat', link:''}]
+            },
+            {
+                nama: "Tata Istilah, Definisi, dan Silogisme",
+                materi: [{nama: 'Pembentukan dan pemakaian istilah', link:''},{nama:'Penyusunan definisi', link:''}, {nama:'Penarikan simpulan', link:''}]
+            },
+            {
+                nama: "Paragraf",
+                materi: [{nama: 'Syarat paragraf', link:''},{nama:'Jenis paragraf', link:''},{nama:'Pengembangan paragraf', link:''}]
+            },
+            {
+                nama: "Keplagiatan",
+                materi: [{nama: 'Jenis keplagiatan', link:''},{nama:'Pencegahan keplagiatan', link:''}]
+            },
+            {
+                nama: "Karya Tulis Ilmiah",
+                materi: [{nama: 'Kriteria ilmiah', link:''},{nama:'Pemilihan topik', link:''}, {nama:'Penentuan tema', link:''}, {nama:'Penyusunan karangan', link:''}]
+            },
+            {
+                nama: "Komponen Karangan",
+                materi: [{nama: 'Komponen pelengkap awal', link:''},{nama:'Kompoonen isi', link:''}, {nama: 'Komponen pelengkap akhir', link:''}]
+            },
+            {
+                nama: "Bagian Utama Karangan",
+                materi: [{nama: 'Bab pendahuluan', link:''},{nama:'Bab permasalahan', link:''},{nama:'Bab analisis', link:''},{nama:'Bab simpulan dan saran', link:''}]
+            },
+            {
+                nama: "Konvensi Naskan",
+                materi: [{nama: 'Pengetikan', link:''},{nama:'Tipografi', link:''},{nama:'Pengutipan', link:''},{nama:'Pustaka', link:''}]
+            }
         ]
     }
 }
