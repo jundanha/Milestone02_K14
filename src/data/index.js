df3cdd7644433aef6cb33589c1a4316f9fceea9d
sumertayogaa <16520482@std.stei.itb.ac.id>
2021-08-15 00:53:02 +0800

    update data

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,11 @@
-const { ttki } = require('./ttki')
-const { kimdasb } = require('./kimdasb')
+const { ttki } = require('./ttki');
+const { kimdasb } = require('./kimdasb');
+const { matematika_1 } = require('./matematika_1');
+const { par } = require('./par');
 
 module.exports = {
-    ttki,
-    kimdasb
-}
+  ttki,
+  kimdasb,
+  matematika_1,
+  par,
+};
--- src/data/matematika_1.js
+++ src/data/matematika_1.js
@@ -1,88 +1,89 @@
 module.exports = {
-    matematika_1:{
-        files: [{nama: "Calculus, 9th Edition, Purcell et al", link: "#"}]
-        subbab: [
-            {
-                nama: "Pendahuluan",
-                materi: [
-                    {nama: "Bilangan Real", link:"#"},
-                    {nama: "Pertaksamaan dan Nilai Mutlak", link:"#"},
-                    {nama: "Sistem Koordinat", link:"#"},
-                    {nama: "Grafik Persamaan", link:"#"},
-                    {nama: "Fungsi dan Grafiknya", link:"#"},
-                    {nama: "Operasi pada Fungsi", link:"#"},
-                    {nama: "Fungsi Trigonometri", link:"#"}
-                ]
-            },
-            {
-                nama: "Limit",
-                materi: [
-                    {nama: "Pengantar limit", link:"#"},
-                    {nama: "Limit Fungsi", link:"#"},
-                    {nama: "Teorema Limit", link:"#"},
-                    {nama: "Limit Fungsi Trigonometri", link:"#"},
-                    {nama: "Limit Tak Hingga dan Limit di Tak Hingga", link:"#"},
-                    {nama: "Kekontinuan", link:"#"}
-                ]
-            },
-            {
-                nama: "Turunan",
-                materi: [
-                    {nama: "Turunan", link:"#"},
-                    {nama: "Aturan Penentuan Turunan", link:"#"},
-                    {nama: "Turunan Fungsi Trigonometri", link:"#"},
-                    {nama: "Aturan Rantai", link:"#"},
-                    {nama: "Turunan Tingkat Tinggi", link:"#"},
-                    {nama: "Turunan Implisit", link:"#"},
-                    {nama: "Laju yang Berkaitan", link:"#"},
-                    {nama: "Diferensial dan Hampiran", link:"#"}
-                ]
-            },
-            {
-                nama: "Penggunaan Turunan",
-                materi: [
-                    {nama: "Maksimum dan Minimum", link:"#"},
-                    {nama: "Kemonotonan dan Kecekungan", link:"#"},
-                    {nama: "Ekstrim Lokal dan Ekstrim pada Selang Buka", link:"#"},
-                    {nama: "Grafik Fungsi dengan Menggunakan Kalkulus", link:"#"},
-                    {nama: "Pemodelan Matematika", link:"#"},
-                    {nama: "Teorema Nilai Rata-Rata untuk Turunan", link:"#"},
-                    {nama: "Anti Turunan", link:"#"},
-                    {nama: "Pengantar Persamaan Diferensial", link:"#"}
-                ]
-            },
-            {
-                nama: "Integral",
-                materi: [
-                    {nama: "Luas Daerah", link:"#"},
-                    {nama: "Integral Tentu", link:"#"},
-                    {nama: "Teorema Dasar Kalkulus Pertama", link:"#"},
-                    {nama: "Teorema Dasar Kalkulus Kedua", link:"#"},
-                    {nama: "Metode Substitusi", link:"#"},
-                    {nama: "Sifat Simetri Integral", link:"#"}
-                ]
-            },
-            {
-                nama: "Penggunaan Integral",
-                materi: [
-                    {nama: "Luas Daerah pada Bidang", link:"#"},
-                    {nama: "Volume Benda Putar: Metode Cakram, Cincin, dan Irisan Sejajar", link:"#"},
-                    {nama: "Volume Benda Putar: Metode Kulit Tabung", link:"#"},
-                    {nama: "Kerja", link:"#"}
-                ]
-            },
-            {
-                nama: "Fungsi Transenden",
-                materi: [
-                    {nama: "Fungsi Logaritma Natural", link:"#"},
-                    {nama: "Fungsi Invers dan Turunannya", link:"#"},
-                    {nama: "Fungsi Eksponen", link:"#"},
-                    {nama: "Fungsi Eksponen dan Logaritma Umum", link:"#"},
-                    {nama: "Pertumbuhan dan Peluruhan Eksponensial", link:"#"},
-                    {nama: "Persamaan Diferensial Linear Orde Satu", link:"#"},
-                    {nama: "Fungsi Invers Trigonometri", link:"#"}
-                ]
-            },
-        ]
-    }
-}
+  matematika_1: {
+    semester: 1,
+    files: [{ nama: 'Calculus, 9th Edition, Purcell et al', link: '#' }],
+    subbab: [
+      {
+        nama: 'Pendahuluan',
+        materi: [
+          { nama: 'Bilangan Real', link: '#' },
+          { nama: 'Pertaksamaan dan Nilai Mutlak', link: '#' },
+          { nama: 'Sistem Koordinat', link: '#' },
+          { nama: 'Grafik Persamaan', link: '#' },
+          { nama: 'Fungsi dan Grafiknya', link: '#' },
+          { nama: 'Operasi pada Fungsi', link: '#' },
+          { nama: 'Fungsi Trigonometri', link: '#' },
+        ],
+      },
+      {
+        nama: 'Limit',
+        materi: [
+          { nama: 'Pengantar limit', link: '#' },
+          { nama: 'Limit Fungsi', link: '#' },
+          { nama: 'Teorema Limit', link: '#' },
+          { nama: 'Limit Fungsi Trigonometri', link: '#' },
+          { nama: 'Limit Tak Hingga dan Limit di Tak Hingga', link: '#' },
+          { nama: 'Kekontinuan', link: '#' },
+        ],
+      },
+      {
+        nama: 'Turunan',
+        materi: [
+          { nama: 'Turunan', link: '#' },
+          { nama: 'Aturan Penentuan Turunan', link: '#' },
+          { nama: 'Turunan Fungsi Trigonometri', link: '#' },
+          { nama: 'Aturan Rantai', link: '#' },
+          { nama: 'Turunan Tingkat Tinggi', link: '#' },
+          { nama: 'Turunan Implisit', link: '#' },
+          { nama: 'Laju yang Berkaitan', link: '#' },
+          { nama: 'Diferensial dan Hampiran', link: '#' },
+        ],
+      },
+      {
+        nama: 'Penggunaan Turunan',
+        materi: [
+          { nama: 'Maksimum dan Minimum', link: '#' },
+          { nama: 'Kemonotonan dan Kecekungan', link: '#' },
+          { nama: 'Ekstrim Lokal dan Ekstrim pada Selang Buka', link: '#' },
+          { nama: 'Grafik Fungsi dengan Menggunakan Kalkulus', link: '#' },
+          { nama: 'Pemodelan Matematika', link: '#' },
+          { nama: 'Teorema Nilai Rata-Rata untuk Turunan', link: '#' },
+          { nama: 'Anti Turunan', link: '#' },
+          { nama: 'Pengantar Persamaan Diferensial', link: '#' },
+        ],
+      },
+      {
+        nama: 'Integral',
+        materi: [
+          { nama: 'Luas Daerah', link: '#' },
+          { nama: 'Integral Tentu', link: '#' },
+          { nama: 'Teorema Dasar Kalkulus Pertama', link: '#' },
+          { nama: 'Teorema Dasar Kalkulus Kedua', link: '#' },
+          { nama: 'Metode Substitusi', link: '#' },
+          { nama: 'Sifat Simetri Integral', link: '#' },
+        ],
+      },
+      {
+        nama: 'Penggunaan Integral',
+        materi: [
+          { nama: 'Luas Daerah pada Bidang', link: '#' },
+          { nama: 'Volume Benda Putar: Metode Cakram, Cincin, dan Irisan Sejajar', link: '#' },
+          { nama: 'Volume Benda Putar: Metode Kulit Tabung', link: '#' },
+          { nama: 'Kerja', link: '#' },
+        ],
+      },
+      {
+        nama: 'Fungsi Transenden',
+        materi: [
+          { nama: 'Fungsi Logaritma Natural', link: '#' },
+          { nama: 'Fungsi Invers dan Turunannya', link: '#' },
+          { nama: 'Fungsi Eksponen', link: '#' },
+          { nama: 'Fungsi Eksponen dan Logaritma Umum', link: '#' },
+          { nama: 'Pertumbuhan dan Peluruhan Eksponensial', link: '#' },
+          { nama: 'Persamaan Diferensial Linear Orde Satu', link: '#' },
+          { nama: 'Fungsi Invers Trigonometri', link: '#' },
+        ],
+      },
+    ],
+  },
+};
