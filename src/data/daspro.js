7b50c7fd0169ed60b20ad46fe71455861f05b2e7
averus9 <16520480@std.stei.itb.ac.id>
2021-08-15 12:08:54 +0700

    daspro

---
+++ src/data/daspro.js
@@ -0,0 +1,24 @@
+module.exports = {
+    daspro: 
+    {
+        files: [{nama:'Buku sakti', link:'drive.com'},{nama: 'Buku besar', link:'sipsipsip'}],
+        subbab: [
+            {
+                nama: "Konsep dan Lingkungan Pemrograman",
+                materi: [{nama: 'What is programming fundamentals concept?', link:''},{nama:'Programming tools and environment', link:''},{nama:'Peran bahasa pemrograman', link:''}]
+            },
+            {
+                nama: "Berpikir Abstrak dan Fungsional untuk Problem Solving",
+                materi: [{nama: 'Berpikir abstrak dan fungsional', link:''},{nama:'Pengantar rekurens', link:''},{nama:'Rekurens dan pemrograman fungsional: Operasi max dan min', link:''},{nama:'Rekurens dan pemrograman fungsional: ekspresi lambda', link:''}]
+            },
+            {
+                nama: "Berpikir Modular, Dekomposisi Modul, dan Pemrograman Prosedural",
+                materi: [{nama: 'Pengenalan pola', link:''},{nama:'Generalisasi pola untuk mendefinisikan abstraksi atau model', link:''},{nama:'Source code standards and best practices of programming', link:''},{nama:'Program taxonomy dan kemampuan programming yang dibutuhkan', link:''}]
+            },
+            {
+                nama: "Pemrograman Prosedural, Keterbatasan, serta Tantangan Memprogram",
+                materi: [{nama: 'Limit of Computing', link:''},{nama:'Kompleksitas dan program serta data skala besar', link:''}, {nama:'Program kompleks dan data besar', link:''}]
+            }
+        ]
+    }
+}
