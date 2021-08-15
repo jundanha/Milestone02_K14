e04c1d1d2830bcde649cb8537a7192f3fcd02d8e
jundanha <jundanha@gmail.com>
2021-08-15 15:17:48 +0700

    update data matematika-2

--- src/data/matematika_2.js
+++ src/data/matematika_2.js
@@ -3,37 +3,61 @@ module.exports = {
     {   
         nama : "KALKULUS IIA", 
         semester: 2,
-        files: [{ nama: 'Calculus, 9th Edition, Purcell et al', link: '#' }],
+        files: [{ nama: 'Calculus, 9th Edition, Purcell et al', link: 'https://drive.google.com/file/d/0B8pw4VeuQQy3OENwREs1TDhOYmM/view?resourcekey=0-F9zr2TjeVPbK0W2sNmDDFg' }],
         subbab: [
             {
                 nama: "Teknik Pengintegralan",
                 materi: [
-                    {nama: "Aturan Dasar Integrasi", link: ""},
-                    {nama: "Integral Parsial", link: ""},
-                    {nama: "Beberapa Integral Trigonometri", link: ""},
-                    {nama: "Merasionalkan Bentuk Akar", link: ""},
-                    {nama: "Integrasi dengan Pecahan Parsial", link: ""}
+                    {nama: "Aturan Dasar Integrasi", link: "https://youtu.be/S6UPXukSsws"},
+                    {nama: "Integral Parsial", link: "https://youtu.be/S6UPXukSsws"},
+                    {nama: "Beberapa Integral Trigonometri Bagian 1", link: "https://youtu.be/0AbvAHk5C40"},
+                    {nama: "Beberapa Integral Trigonometri Bagian 2", link: "https://youtu.be/qwDznWsvQZ4"}
                 ]
             },
             {
-                nama: "Integral Tak-Wajar",
+                nama: "Integral TakWajar",
                 materi: [
-                    {nama: "Bentuk Tak Tentu Tipe 0/0 dan ∞/∞", link: ""},
-                    {nama: "Bentuk Tak Tentu Lain", link: ""},
-                    {nama: "Bentuk Tak Wajar", link: ""}
+                    {nama: "Integral Takwajar 1", link: "https://youtu.be/nTq_XPOZDZY"},
+                    {nama: "Integran Takterbatar", link: "https://youtu.be/4MVhSfoqPOw"}
                 ]
 
             },
             {
                 nama: "Deret Tak-Hingga",
                 materi: [
-                    {nama: "Barisan Tak Hingga", link: ""},
-                    {nama: "Deret Tak Hingga", link: ""},
-                    {nama: "Deret Positif", link: ""},
-                    {nama: "Deret Ganti-Tanda", link: ""},
-                    {nama: "Deret Pangkat", link: ""},
-                    {nama: "Operasi pada Deret Pangkat", link: ""},
-                    {nama: "Deret Taylor dan Maclaurin", link: ""},
+                    {nama: "Barisan", link: "https://youtu.be/wijEDnObk0g"},
+                    {nama: "Barisan Konvergen", link: "https://youtu.be/uDecBHAAIpY"},
+                    {nama: "Aritmatika Limit", link: "https://youtu.be/41sH92QfUuA"},
+                    {nama: "Teorema Apit", link: "https://youtu.be/EyVpTO60xbg"},
+                    {nama: "Teorema Kemonotonan", link: "https://youtu.be/EyVpTO60xbg"},
+                    {nama: "Deret dan Jumlah Parsial", link: "https://youtu.be/NgBVW9fYxJc"},
+                    {nama: "Uji Kedivergenan dan Deret Harmonik", link: "https://youtu.be/4PKYzkLC2_s"},
+                    {nama: "Deret Teleskoping", link: "https://youtu.be/_fbtyiTudK4" },
+                    {nama: "Beberapa Sifat Deret", link: "https://youtu.be/IqQ9q0jm1Aw"},
+                    {nama: "Uji Jumlah Terbatas", link: "https://youtu.be/22IheVDkUfw"},
+                    {nama: "Uji Integral", link: "https://youtu.be/8bSBE28eI-Q"},
+                    {nama: "Uji Integral dan Deret-p", link: "https://youtu.be/IiOj8-y8olo"},
+                    {nama: "Menaksir Jumlah Deret", link: "https://youtu.be/uXx53IFy7cY"},
+                    {nama: "Uji Banding Biasa", link: "https://youtu.be/ipIYMaUAy5Q"},
+                    {nama: "Uji Banding Limit", link: "https://youtu.be/YyW8MoRKJBw"},
+                    {nama: "Uji Rasio", link: "https://youtu.be/8GwCfKOT0Z0"},
+                    {nama: "Uji Banding Biasa (Advenced)", link: "https://youtu.be/GsdyclrGFl4"},
+                    {nama: "Deret Ganti Tanda", link: "https://youtu.be/R7-jiCwfprc"},
+                    {nama: "Taksiran Galat Deret Berayun", link: "https://youtu.be/FYthwf8u1a8"},
+                    {nama: "Uji Kekonvergenan Mutlak", link: "https://youtu.be/g6LAn1IAMOo"},
+                    {nama: "Uji Rasio Mutlak", link: "https://youtu.be/KvkKxd8uPuQ"},
+                    {nama: "Perbedaan Konvergen Mutlat dengan Konvergen Bersyarat", link: "https://youtu.be/-MsHyuXJGUA"},
+                    {nama: "Deret Pangkat", link: "https://youtu.be/3WxxzbXmomo"},
+                    {nama: "Deret Pangkat dalam (x-a)", link: "https://youtu.be/UAuzPdot0gQ"},
+                    {nama: "Manipulasi Deret Pangkat", link: "https://youtu.be/1LN3A_hiAlM"},
+                    {nama: "Implikasi Manipulasi Deret Pangkat", link: "https://youtu.be/6-83EBDqBu4"},
+                    {nama: "Operasi Deret Pangkat", link: "https://youtu.be/mGOUbxmyp4M"},
+                    {nama: "Deret Taylor dan Maclaurin (Bagian 1)", link: "https://youtu.be/0m9ZJWMTr_w"},
+                    {nama: "Deret Taylor dan Maclaurin (Bagian 2)", link: "https://youtu.be/qa6u8GuAjhE"},
+                    {nama: "Deret Binomial", link: "https://youtu.be/kDgPLYni-sU"},
+                    {nama: "Hampiran dengan Polinom Taylor", link: "https://youtu.be/vQuB2zYxty4"},
+                    {nama: "Polinom Maclaurin", link: "https://youtu.be/Lt4c2tFkDNQ"},
+                    {nama: "Analisis Galat dalam Aproksimasi Taylor", link: "https://youtu.be/BX0VMUOKE6k"}
                 ]
 
             },
